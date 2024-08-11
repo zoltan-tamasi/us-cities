b4bb3d7eed9d0e5807f3ab85c842f4397124e1c3
Zoltan Tamasi <tzoltan84@gmail.com>
2024-08-11 14:28:16 +0200

    Improve error handling code

--- src/main/scala/net/zoltantamasi/uscities/Main.scala
+++ src/main/scala/net/zoltantamasi/uscities/Main.scala
@@ -1,17 +1,32 @@
 package net.zoltantamasi.uscities
 
 import scala.io.Source
+import scala.util.{Try, Success, Failure}
 import upickle.default._
 import scala.collection.immutable.TreeMap
 
 object UsCities {
-  def main(args: Array[String]): Unit = {
 
-    val filename = "uscities.csv"
-    val delimiter = ","
-    val file = Source.fromFile(filename)
+  val filename = "uscities.csv"
+  val delimiter = ","
+
+  def readCity(line: String): Try[City] = {
+    val fields = line.split(delimiter).map(_.trim).map(_.replace("\"", ""))
+      
+    Try {
+
+      val name = fields(1)
+      val lat = fields(6).toFloat
+      val lng = fields(7).toFloat
+      val id = fields(16).toInt
+
+      City(id.toInt, name, lat.toFloat, lng.toFloat)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
     
-    var wrongLines: List[String] = List[String]()
+    val file = Source.fromFile(filename)
     
     var cities: List[City] = List[City]()
 
@@ -19,27 +34,21 @@ object UsCities {
     var citiesLatMap: TreeMap[Float, City] = TreeMap[Float, City]()
     var citiesLngMap: TreeMap[Float, City] = TreeMap[Float, City]()
 
-
     for (line <- file.getLines().drop(1)) {
-      val fields = line.split(delimiter).map(_.trim).map(_.replace("\"", ""))
-      
-      try {
-        val name = fields(1)
-        val lat = fields(6).toFloat
-        val lng = fields(7).toFloat
-        val id = fields(16).toInt
+     
+      val city = readCity(line);
 
-        val city = City(id.toInt, name, lat.toFloat, lng.toFloat)
+      city match {
+        case Success(city) =>
 
-        citiesIdMap = citiesIdMap.updated(id.toInt, city)
-        citiesLatMap = citiesLatMap.updated(lat, city)
-        citiesLngMap = citiesLngMap.updated(lng, city)
+          citiesIdMap = citiesIdMap.updated(city.id, city)
+          citiesLatMap = citiesLatMap.updated(city.lat, city)
+          citiesLngMap = citiesLngMap.updated(city.lng, city)
+    
+          cities = cities :+ city
 
-        cities = cities :+ City(id.toInt, name, lat.toFloat, lng.toFloat)
-      } catch {
-        case _: Exception => {
-          wrongLines = wrongLines :+ line
-        }
+        case Failure(error) =>
+          println(s"Error reading line: $line \n Error is $error")
       }
     }
 
@@ -57,7 +66,7 @@ object UsCities {
     val t4 = System.currentTimeMillis()
     val filterTime = t4 - t3
 
-    wrongLines.foreach(s => println(s"wrong line: $s"))
+    
     file.close()
     
     println(s"filter time: $filterTime")
