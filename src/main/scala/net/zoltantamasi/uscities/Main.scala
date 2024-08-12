4e9f4309acc3da2c240d074427b047128b58e261
Zoltan Tamasi <tzoltan84@gmail.com>
2024-08-12 07:52:15 +0200

    Improve algo type handling

--- src/main/scala/net/zoltantamasi/uscities/Main.scala
+++ src/main/scala/net/zoltantamasi/uscities/Main.scala
@@ -9,6 +9,12 @@ object UsCities {
 
   val filename = "uscities.csv"
   val delimiter = ","
+  val ALGO_TYPE = scala.util.Properties.envOrElse("ALGO", "TREE").trim
+
+  if (!ALGO_TYPE.equals("LIST") && !ALGO_TYPE.equals("TREE")) {
+    println(s"Value of ALGO must be TREE or LIST, but was: $ALGO_TYPE");
+    System.exit(0)
+  }
 
   def readCity(line: String): Try[City] = {
     val fields = line.split(delimiter).map(_.trim).map(_.replace("\"", ""))
@@ -56,27 +62,33 @@ object UsCities {
         .toList
         .flatten
 
-    //val lngOrdered = cities.sorted(CityLngOrdering)
-    //val latOrdered = cities.sorted(CityLatOrdering)
-
-    val t3 = System.currentTimeMillis()
-
-    val closeCities = 
-      cities
-        //.map(City.getCloseCities(latOrdered, lngOrdered, _))
-        .map(city => City.getCloseCitiesByMap(citiesLatMap, citiesLngMap, citiesIdMap, city.id))
-        .map(City.calculateDistances(_))
-
-    val t4 = System.currentTimeMillis()
-    val filterTime = t4 - t3
-
-    
     file.close()
+        
+    val start = System.currentTimeMillis()
+
+    val closeCities = ALGO_TYPE match {
+      case "LIST" =>
+
+        val lngOrdered = cities.sorted(CityLngOrdering)
+        val latOrdered = cities.sorted(CityLatOrdering)
+        
+        cities
+          .map(City.getCloseCities(latOrdered, lngOrdered, _))
+          .map(City.calculateDistances(_))
+
+      case "TREE" => 
+        cities
+          .map(city => City.getCloseCitiesByMap(citiesLatMap, citiesLngMap, citiesIdMap, city.id))
+          .map(City.calculateDistances(_))
+    }
+
+    val end = System.currentTimeMillis()
+    val time = end - start
+    val count = cities.length
     
-    println(s"filter time: $filterTime")
+    println(s"$count cities processed with algo: $ALGO_TYPE in: $time ms")
 
     os.write.over(os.pwd / "us-close-cities.json", upickle.default.write(closeCities))
-
   }
 }
 
